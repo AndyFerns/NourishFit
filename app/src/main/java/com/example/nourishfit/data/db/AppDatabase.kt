ae44316f3ee8378b7c22a4942fdfe7ab78fdfc57
AndyFerns <write2andrew.fernandes@gmail.com>
2025-09-14 18:06:05 +0530

    removed auto migration

--- app/src/main/java/com/example/nourishfit/data/db/AppDatabase.kt
+++ app/src/main/java/com/example/nourishfit/data/db/AppDatabase.kt
@@ -6,10 +6,7 @@ import androidx.room.RoomDatabase
 
 @Database(
     entities = [FoodEntity::class],
-    version = 2,
-    autoMigrations = [
-        AutoMigration (from = 1, to = 2)
-    ]
+    version = 1
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun foodDao(): FoodDao
