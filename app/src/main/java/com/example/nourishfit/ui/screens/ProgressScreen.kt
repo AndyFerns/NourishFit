1692ec0359aa94b9c420def5e19a4cdbb6f90288
AndyFerns <write2andrew.fernandes@gmail.com>
2025-10-06 00:27:27 +0530

    added extra import

--- app/src/main/java/com/example/nourishfit/ui/screens/ProgressScreen.kt
+++ app/src/main/java/com/example/nourishfit/ui/screens/ProgressScreen.kt
@@ -1,5 +1,6 @@
 package com.example.nourishfit.ui.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -13,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.draw.clip
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
