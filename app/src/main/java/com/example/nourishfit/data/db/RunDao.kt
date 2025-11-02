fe9a0f63f4f95437b415a7028bf38f475dbc3e9b
AndyFerns <write2andrew.fernandes@gmail.com>
2025-11-02 12:24:51 +0530

    New file for tracking runs and Getting Runs for a specific user

---
+++ app/src/main/java/com/example/nourishfit/data/db/RunDao.kt
@@ -0,0 +1,17 @@
+package com.example.nourishfit.data.db
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface RunDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRun(run: RunEntity)
+
+    // Gets all runs for a specific user, ordered from newest to oldest
+    @Query("SELECT * FROM runs WHERE userId = :userId ORDER BY timestamp DESC")
+    fun getAllRunsByUser(userId: String): Flow<List<RunEntity>>
+}
