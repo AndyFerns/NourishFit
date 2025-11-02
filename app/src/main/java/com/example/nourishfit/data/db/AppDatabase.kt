30be384481a20d97a41c78bb6704ea4f3ec7a128
AndyFerns <write2andrew.fernandes@gmail.com>
2025-11-02 12:11:53 +0530

    added RunEntity and @TypeConverters prefix as a stub for future implementation

--- app/src/main/java/com/example/nourishfit/data/db/AppDatabase.kt
+++ app/src/main/java/com/example/nourishfit/data/db/AppDatabase.kt
@@ -3,11 +3,17 @@ package com.example.nourishfit.data.db
 import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 @Database(
-    entities = [FoodEntity::class],
-    version = 1
+    entities = [
+        FoodEntity::class,
+        RunEntity::class
+    ],
+    version = 2
 )
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun foodDao(): FoodDao
+    abstract fun runDao(): RunDao
 }
